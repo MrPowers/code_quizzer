2f9524988259d96cf4edd6c78f5b2e3eb585329c
Matthew Powers <matthewkevinpowers@gmail.com>
2012-11-11 16:32:04 -0800

    added devise, added bootstrap views, last commit before I generate the test and question models and then implement authorization

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 CompoundBlingApp::Application.routes.draw do
+  devise_for :users
+
+  root :to => 'welcome#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
