db45f1629d1d614228c5adb109673d41f7025c4d
Matthew Powers <matthewkevinpowers@gmail.com>
2012-11-29 09:50:55 -0800

    updated ability class because it was not identifying users that were not logged in properly.  when i called user.new, the user was populated with a role of author because this is the default value, so uses that are not logged in were given too many rights.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,26 +3,24 @@ class Ability
 
   def initialize(user)
     # Define abilities for the passed in user here. For example:
-    #
-      user ||= User.new # guest user (not logged in)
-      if user.role == "admin"
-        can :manage, :all
-      else
-        can :read, :all
-        cannot :destroy, [Question, Quiz]
-        cannot :update, [Question, Quiz]
-        cannot :create, [Question, Quiz]
-        if user.role == "author"
-            can :create, Quiz
-            can :create, Question, :quiz => { :user_id => user.id } #author can update if user_id in quizzes table equals the author's user_id
-            can :destroy, Question do |question|
-                question.try(:user) == user
-            end
-            can :update, Question do |question|
-                question.try(:user) == user
-            end
-        end
+    if user.nil?
+      can :read, :all
+      cannot :destroy, [Question, Quiz]
+      cannot :update, [Question, Quiz]
+      cannot :create, [Question, Quiz]
+    elsif user.role == "admin"
+      can :manage, :all
+    elsif user.role == "author"
+      can :create, Quiz
+      can :create, Question, :quiz => { :user_id => user.id } #author can update if user_id in quizzes table equals the author's user_id
+      can :destroy, Question do |question|
+          question.try(:user) == user
       end
+      can :update, Question do |question|
+          question.try(:user) == user
+      end
+    end
+  end
     #
     # The first argument to `can` is the action you are giving the user permission to do.
     # If you pass :manage it will apply to every action. Other common actions here are
@@ -37,5 +35,4 @@ class Ability
     #   can :update, Article, :published => true
     #
     # See the wiki for details: https://github.com/ryanb/cancan/wiki/Defining-Abilities
-  end
 end
