6b1f3db283f80ff1744dbfa5cb148a1dac731844
MrPowers <matthewkevinpowers@gmail.com>
2013-06-29 20:21:32 -0400

    Add seed file for development database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+user = User.create email: "bob@example.com", password: "Password1", password_confirmation: "Password1"
 
+f1 = Family.create name: "Programming Family"
+Family.create name: "Books Family"
+
+t1 = f1.topics.create! name: "Rails Topic", user_id: user.id
+
+quiz1 = t1.quizzes.create! body: "MVC Quiz", user_id: user.id
+
+quiz1.questions.create! body: "Knock knock", answer: "Who's there", user_id: user.id
