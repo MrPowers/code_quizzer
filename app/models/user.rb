4b24cff4f831b9a5aeda67f926cbb638e6ccbebd
MrPowers <matthewkevinpowers@gmail.com>
2013-12-01 21:30:32 -0500

    Add username to User model.  Update comments on the Quiz#show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :role
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :role, :username
+
+  validates :username, :uniqueness => true
 
   has_many :quizzes
   has_many :questions
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131201225254) do
+ActiveRecord::Schema.define(:version => 20131202005311) do
 
   create_table "comments", :force => true do |t|
     t.text     "body"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(:version => 20131201225254) do
     t.datetime "created_at",                                   :null => false
     t.datetime "updated_at",                                   :null => false
     t.string   "role",                   :default => "author"
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
