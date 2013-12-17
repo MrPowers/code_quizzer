f57fdb7457369f4de77361a5a1c80c3a4de8b7b2
MrPowers <matthewkevinpowers@gmail.com>
2013-12-17 13:57:25 -0500

    Add authorization for ExamsController

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -1,6 +1,7 @@
 class ExamsController < ApplicationController
   def grade_exam
     @exam = Exam.find(params[:id])
+    authorize! :update, @exam
     @exam.grade
     respond_to do |format|
       format.json { render :json => @exam }
