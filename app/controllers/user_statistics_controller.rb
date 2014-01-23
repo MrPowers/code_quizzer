63c66393612666adcc55cfe5edf3b45c449cdff4
MrPowers <matthewkevinpowers@gmail.com>
2014-01-23 18:51:43 -0500

    Add graded_exams and cancelled_exams to /stats view

--- app/controllers/user_statistics_controller.rb
+++ app/controllers/user_statistics_controller.rb
@@ -1,7 +1,13 @@
 class UserStatisticsController < ApplicationController
   def index
-    users = User.where(created_at: 90.days.ago..Time.now).order("created_at desc")
-    users_by_date = users.group_by {|users| users.created_at.to_date}
-    @grouped_users = users_by_date.inject({}) {|memo, (date, users)| memo[date] = users.count; memo}
+    @grouped_users = grouped_count(User, {created_at: 90.days.ago..Time.now})
+    @grouped_graded_exams = grouped_count(Exam, {created_at: 90.days.ago..Time.now, status: "graded"})
+    @grouped_cancelled_exams = grouped_count(Exam, {created_at: 90.days.ago..Time.now, status: "cancelled"})
+  end
+
+  def grouped_count(klass, query_args)
+    records = klass.where(query_args).order("created_at desc")
+    by_date = records.group_by {|record| record.created_at.to_date}
+    by_date.inject({}) {|memo, (date, record)| memo[date] = record.count; memo}
   end
 end
