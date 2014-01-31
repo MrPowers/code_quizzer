b8f196af03c9df1907ab4d45fce44acb6931b51a
MrPowers <matthewkevinpowers@gmail.com>
2014-01-30 22:53:27 -0500

    Delete unused QuizzesController actions and associated views.  Quiz creation will now take place on the command line to reduce code complexity

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,42 +1,13 @@
 class QuizzesController < ApplicationController
-  load_and_authorize_resource :topic
-  load_and_authorize_resource :quiz, :through => :topic, :shallow => true
-  def new
-  end
-
-  def create
-    @quiz.user_id = current_user.id
-    if @quiz.save
-      redirect_to @quiz.topic.family
-    end
-  end
-
-  def index
-  end
-
   def show
+    @topic = Topic.find(params[:topic_id])
+    authorize! :show, @topic
+    @quiz = Quiz.find(params[:id])
+    authorize! :show, @quiz
     if current_user
       authorize! :create, Exam
       @exam = Exam.where(:quiz_id => @quiz.id, :user_id => current_user.id).where("status IS NULL").order("id desc").first_or_create
       authorize! :update, @exam
     end
-  end
-
-  def edit
-  end
-
-  def update
-    if @quiz.update_attributes(params[:quiz])
-      redirect_to topics_path
-    end
-  end
-
-  def destroy
-    @quiz.destroy
-    redirect_to topics_path
-  end
-
-  def admin_update
-    @quiz = Quiz.find(params[:id])
   end
 end
