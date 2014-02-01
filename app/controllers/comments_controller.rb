5233c93a955905074c90313ac0bb7fee70085c60
MrPowers <matthewkevinpowers@gmail.com>
2014-02-01 12:19:06 -0500

    Fix bugs from the refactoring to add the topics and quizzes slugs to the URL instead of the parameters

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,9 @@
 class CommentsController < ApplicationController
   def create
-    @quiz = Quiz.find(params[:quiz_id])
+    @quiz = Quiz.where(slug: params[:quiz_id]).first
     @comment = Comment.new(params[:comment].merge({ :quiz_id => @quiz.id, :user_id => current_user.id }))
     if @comment.save
-      redirect_to topic_quiz_path(topic_id: @quiz.topic.id, id: @quiz, anchor: "new_comment")
+      redirect_to topic_quiz_path(topic_id: @quiz.topic.slug, id: @quiz.slug, anchor: "new_comment")
     end
   end
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,38 +1,37 @@
 class QuestionsController < ApplicationController
-  before_filter :find_quiz
   before_filter :find_question, :except => [:create]
 
   def create
+    @quiz = Quiz.where(slug: params[:quiz_id]).first
     @question = @quiz.questions.new(params[:question])
     @question.user = current_user
     authorize! :create, @question
     if @question.save
-      redirect_to topic_quiz_path(topic_id: @quiz.topic.id, id: @quiz, anchor: "new_question")
+      redirect_to topic_quiz_path(topic_id: @quiz.topic.slug, id: @quiz, anchor: "new_question")
     end
   end
 
   def edit
+    @quiz = Quiz.find(params[:quiz_id])
     authorize! :update, @question
   end
 
   def update
+    @quiz = Quiz.where(slug: params[:quiz_id]).first
     authorize! :update, @question
     if @question.update_attributes(params[:question])
-      redirect_to topic_quiz_path(topic_id: @quiz.topic.id, id: @quiz)
+      redirect_to topic_quiz_path(topic_id: @quiz.topic.slug, id: @quiz)
     end
   end
 
   def destroy
+    @quiz = Quiz.where(slug: params[:quiz_id]).first
     authorize! :destroy, @question
     @question.destroy
-    redirect_to topic_quiz_path(topic_id: @quiz.topic.id, id: @quiz)
+    redirect_to topic_quiz_path(topic_id: @quiz.topic.slug, id: @quiz)
   end
 
   private
-
-  def find_quiz
-    @quiz = Quiz.find(params[:quiz_id])
-  end
 
   def find_question
     @question = Question.find(params[:id])
