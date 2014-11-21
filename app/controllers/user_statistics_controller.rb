e7c91fc482b43392bddfc8b29c53d4202cf9e504
Matthew Powers <matthewkevinpowers@gmail.com>
2014-11-21 09:00:16 -0500

    Update user_statistics controller for the updated Exam schema, but the grouped counts still seem to be behaving oddly

--- app/controllers/user_statistics_controller.rb
+++ app/controllers/user_statistics_controller.rb
@@ -2,12 +2,12 @@ class UserStatisticsController < ApplicationController
   def index
     @total_users = User.all.count
     @total_exams = Exam.all.count
-    @graded_exams = Exam.where(:status => "graded").count
-    @cancelled_exams = Exam.where(:status => "cancelled").count
+    @graded_exams = Exam.where(:is_graded => true).count
+    @cancelled_exams = Exam.where(:is_canceled => true).count
 
     @grouped_users = grouped_count(User, {created_at: 90.days.ago..Time.now})
-    @grouped_graded_exams = grouped_count(Exam, {created_at: 90.days.ago..Time.now, status: "graded"})
-    @grouped_cancelled_exams = grouped_count(Exam, {created_at: 90.days.ago..Time.now, status: "cancelled"})
+    @grouped_graded_exams = grouped_count(Exam, {created_at: 90.days.ago..Time.now, is_graded: true})
+    @grouped_cancelled_exams = grouped_count(Exam, {created_at: 90.days.ago..Time.now, is_canceled: true})
   end
 
   def grouped_count(klass, query_args)
