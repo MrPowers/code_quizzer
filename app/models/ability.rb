5ece1384784f5270604d2048729d0fa5536fa807
Matthew Powers <matthewkevinpowers@gmail.com>
2014-11-22 07:05:23 -0500

    Delete the Family records that are not being used anymore

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,13 +4,11 @@ class Ability
     # Users that are not logged in don't exist so they are nil.  These users can only read.
     if user.nil?
       can :read, :all
-      cannot :read, Family.where("name = ? OR name = ?", "Books", "Caitlin")
     # Admin users can read, create, and delete anything.
     elsif user.role == 'admin'
       can :manage, :all
     elsif user.role == 'author'
       can :read, :all
-      cannot :read, Family.where("name = ? OR name = ?", "Books", "Caitlin")
       can :create, Comment
       can :create, Exam
       can :update, Exam do |exam|
