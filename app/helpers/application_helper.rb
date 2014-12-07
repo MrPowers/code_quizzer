10486429d32e7725837178fa0834b3f7418e3dce
Matthew Powers <matthewkevinpowers@gmail.com>
2014-12-07 14:13:42 -0500

    Finish FontAwesome blog post

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def markdownify(str)
-    @markdown ||= Redcarpet::Markdown.new(HTMLwithPygments, fenced_code_blocks: true, no_intra_emphasis: true)
-    find_and_preserve(@markdown.render(str)).html_safe
+    markdown ||= Redcarpet::Markdown.new(HTMLwithPygments, fenced_code_blocks: true, no_intra_emphasis: true)
+    markdown.render(str).html_safe
   end
 end
