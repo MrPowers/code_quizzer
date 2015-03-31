c1f593b50b9f1173fce3726e8f85b3909c7d5c9b
Matthew Powers <matthewkevinpowers@gmail.com>
2015-03-30 20:05:49 -0500

    Update the seeding process, so it actually works and developers can get the application up-and-running on their machine

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,12 @@
-# bare bones seeds
+family = Family.create!(
+  name: "Programming",
+  description: "Ruby, Rails and JavaScript practice problems",
+  page_title: "Learn Ruby, Rails, JavaScript | How to code",
+  slug: "learn-programming"
+)
 
-author = User.create email: 'bob@example.com', password: 'Password1', password_confirmation: 'Password1', username: Faker::Internet.user_name
-
-# create admin
 admin = User.create email: 'you@example.com', password: 'password', password_confirmation: 'password', username: Faker::Internet.user_name, role: 'admin'
 
-f1 = Family.create name: 'Programming Family', description: 'A family description'
-t1 = f1.topics.create! name: 'Rails Topic', user_id: admin.id, slug: 'topic-slug', family_id: f1.id
-quiz1 = t1.quizzes.create! body: 'MVC Quiz', user_id: admin.id, slug: 'quiz-slug', description: 'A first quiz', page_title: 'a quiz page title'
-quiz1.questions.create! body: 'Knock knock', answer: "Who's there", user_id: admin.id
+topic = family.topics.create! name: 'Rails Topic', slug: 'topic-slug', family_id: family.id
+quiz1 = topic.quizzes.create! body: 'MVC Quiz', slug: 'quiz-slug', description: 'A first quiz', page_title: 'a quiz page title'
+quiz1.questions.create! body: 'Knock knock', answer: "Who's there"
